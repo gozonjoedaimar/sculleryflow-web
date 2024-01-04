852c43c0f31f5c33570dc2bb2b2539085df291d3
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-01-05 01:22:39 +0800

    Use remix standards

--- app/routes/menu_.item.tsx
+++ app/routes/menu_.item.tsx
@@ -1,4 +1,12 @@
+import { LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { checkAuth } from "app/hooks/auth";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+    const { authenticated } = await checkAuth(request);
+    if (!authenticated) return redirect('/login');
+    return json({ authenticated });
+}
 
 export default function MenuItem()
 {
