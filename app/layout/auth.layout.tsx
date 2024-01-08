551abbbe1a084444518fa017495d608417797dc6
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-01-08 20:48:54 +0800

    SIde nav theme

--- app/layout/auth.layout.tsx
+++ app/layout/auth.layout.tsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, useNavigation } from "@remix-run/react";
+import { Form, NavLink, Outlet, useNavigation } from "@remix-run/react";
 import { side_nav } from "app/config/navigation";
 import useAuth from "app/hooks/auth";
 import { twMerge } from "tailwind-merge";
@@ -9,18 +9,21 @@ export default function AuthLayout() {
     return (
         !authenticated ?
         null:
-        <div className="dashboard-layout h-full flex flex-row">
-            <div className="sidebar w-48 flex flex-col border-r pr-2">
-                <div className="app-info border-b">
-                    <h1>Dashboard</h1>
+        <div className="dashboard-layout h-full flex flex-row bg-teal-950">
+            <div className="sidebar w-72 flex flex-col p-7 pr-0 text-white">
+                <div className="app-info text-2xl mb-8">
+                    <h1>Sculleryflow</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex flex-col gap-3">
                         {
                             Object.keys(side_nav).map( (key) => {
-                                return <li key={key}>
-                                    <Link to={side_nav[key].path}>{side_nav[key].name}</Link>
-                                </li>
+                                const path = side_nav[key].path;
+                                const name = side_nav[key].name;
+                                return <li key={key}><NavLink to={path} className={ ({ isActive }) => twMerge(
+                                    isActive && "font-bold text-orange-500 border-r-4 border-orange-500",
+                                    "block w-full py-1 hover:text-orange-500 hover:border-r-4 hover:border-orange-500"
+                                ) }>{name}</NavLink></li>;
                             })
                         }
                     </ul>
@@ -28,10 +31,12 @@ export default function AuthLayout() {
             </div>
             <div className={twMerge(
                 "content-area",
-                "w-full",
-                navigation.state === 'loading' && 'opacity-50 pointer-events-none'
+                "w-full p-7 rounded-tl-3xl rounded-bl-3xl bg-teal-50",
             )}>
-                <header className="flex flex-row justify-between border-b">
+                <header className={twMerge(
+                    navigation.state === 'loading' && 'opacity-50 pointer-events-none',
+                    "flex flex-row justify-between border-b"
+                )}>
                     <div className="page-info">
                         <h2>Dashboard</h2>
                     </div>
@@ -45,7 +50,9 @@ export default function AuthLayout() {
                         </ul>
                     </nav>
                 </header>
-                <main>
+                <main className={twMerge(
+                    navigation.state === 'loading' && 'opacity-50 pointer-events-none'
+                )}>
                     <Outlet />
                 </main>
             </div>
