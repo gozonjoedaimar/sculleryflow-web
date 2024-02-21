8d91f65a3745b039e7900b253aecd901d2ab13e7
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-02-21 17:02:50 +0800

    Update button labels in Form component

--- app/components/button_icons/Check.tsx
+++ app/components/button_icons/Check.tsx
@@ -1,15 +1,29 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-type CheckProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+type CheckProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
+    label?: string;
+}
 
-export default function Check({ onClick, type = "button", ...props }: CheckProps)
+export default function Check({ label = "Check", onClick, type = "button", ...props }: CheckProps)
 {
     return (
-        <button {...props} type={type} onClick={onClick} className="w-5 h-5 rounded-full p-0 bg-green-600 hover:bg-green-500 disabled:opacity-50 disabled:hover:bg-green-600 disabled:pointer-events-none">
+        <button {...props} type={type} onClick={onClick} className={twMerge(
+            "w-5 h-5 flex flex-row justify-center items-center rounded-full p-0 group bg-green-600",
+            "hover:bg-green-500 hover:px-1 hover:w-fit",
+            "active:bg-green-500 active:px-1 active:w-fit",
+            "focus:bg-green-500 focus:px-1 focus:w-fit",
+            "disabled:opacity-50 disabled:hover:bg-green-600 disabled:pointer-events-none",
+        )}>
             <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-3/4 h-3/4 m-auto">
                 <title>Save</title>
                 <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#fff" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
+            <span className={twMerge(
+                "hidden w-0 text-xs text-white transition-all",
+                "group-hover:w-fit group-focus:w-fit group-active:w-fit",
+                "group-hover:inline group-focus:inline group-active:inline",
+            )}>{ label }</span>
         </button>
     );
 }
--- app/components/button_icons/Edit.tsx
+++ app/components/button_icons/Edit.tsx
@@ -1,16 +1,30 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-type CloseProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+type CloseProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
+    label?: string;
+}
 
-export default function Edit({ onClick, type="button", ...props } : CloseProps)
+export default function Edit({ label = "Edit", onClick, type="button", ...props } : CloseProps)
 {
     return (
-        <button {...props} type={type} onClick={onClick} className="w-5 h-5 rounded-full p-0 bg-blue-800 hover:bg-blue-700">
-            <svg width="12px" height="12px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 m-auto">
+        <button {...props} type={type} onClick={onClick} className={twMerge(
+            "w-5 h-5 flex flex-row justify-center items-center group rounded-full p-0",
+            "bg-blue-800",
+            "hover:w-fit hover:px-1 hover:bg-blue-700",
+            "focus:w-fit focus:px-1 focus:bg-blue-700",
+            "active:w-fit active:px-1 active:bg-blue-700"
+        )}>
+            <svg width="12px" height="12px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-3 h-3">
                 <title>Edit</title>
                 <path d="M20.1497 7.93997L8.27971 19.81C7.21971 20.88 4.04971 21.3699 3.27971 20.6599C2.50971 19.9499 3.06969 16.78 4.12969 15.71L15.9997 3.84C16.5478 3.31801 17.2783 3.03097 18.0351 3.04019C18.7919 3.04942 19.5151 3.35418 20.0503 3.88938C20.5855 4.42457 20.8903 5.14781 20.8995 5.90463C20.9088 6.66146 20.6217 7.39189 20.0997 7.93997H20.1497Z" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M21 21H12" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
+            <span className={twMerge(
+                "hidden w-0 text-xs text-white transition-all",
+                "group-hover:w-fit group-focus:w-fit group-active:w-fit",
+                "group-hover:inline group-focus:inline group-active:inline",
+            )}>{ label }</span>
         </button>
     );
 }
